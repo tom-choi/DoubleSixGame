2bb46880e09493731f0e2df96d52d901876b6d68
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-04-06 14:11:47 +0800

    AIPlayer 重構

--- Assets/Scripts/Player/AIPlayer.cs
+++ Assets/Scripts/Player/AIPlayer.cs
@@ -4,28 +4,69 @@ using UnityEngine;
 
 public class AIPlayer : MonoBehaviour
 {
-    public int currentPosition = 0;
-    public int targetPosition = 10;
+    public MMap map;
+    public float moveSpeed = 2.5f;
+    public float moveTime = 0.75f;
 
+    private MapNode currentNode;
+    
     public Dice dice;
+
+    //其他属性和方法
 
     void Start()
     {
-
+        map.GenerateMap();
+        currentNode = map.firstNode;
+        transform.position = currentNode.position + new Vector3(0,0.5f,0);
     }
 
     void Update()
     {
-        Move(dice.GetResult());
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveToNextNode();
+        }
     }
 
-    public void Move(int steps)
+    public IEnumerator WaitForRollDice()
     {
-        currentPosition += steps;
+        // Wait for the player to click the "Roll Dice" button
+        // while (!gameController.diceRolled)
+        // {
+        //     yield return null;
+        // }
+        yield return null;
+    }
 
-        if (currentPosition >= targetPosition)
+    void MoveToNextNode()
+    {
+        if (!currentNode.NextNodesIsEmpty())
         {
-            Debug.Log("Game Over!");
+            currentNode = currentNode.GetRandomNextNode();
+            Debug.Log(currentNode.position);
+            Vector3 targetPosition = currentNode.position + new Vector3(0,0.5f,0);
+            StartCoroutine(MoveToNode(targetPosition));
         }
+    }
+
+    IEnumerator MoveToNode(Vector3 targetPosition)
+    {
+        // 等待 x 秒，使玩家停顿一下
+        yield return new WaitForSeconds(moveTime);
+
+        // 移动玩家到目标位置
+        float journeyLength = Vector3.Distance(transform.position, targetPosition);
+        float startTime = Time.time;
+        float speed = moveSpeed;
+
+        while (transform.position != targetPosition)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, fracJourney);
+            yield return null;
+        }
+
     }
 }
