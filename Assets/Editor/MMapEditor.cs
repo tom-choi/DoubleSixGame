5178217b5b44844a58e4155c0259ac33d652931f
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-04-06 23:49:17 +0800

    OnSceneGUI 測試 Handles.DrawWireDisc

--- Assets/Editor/MMapEditor.cs
+++ Assets/Editor/MMapEditor.cs
@@ -19,6 +19,28 @@ public class MMapEditor : Editor
         mapObjectProp = mMapObject.FindProperty("mapObject");
     }
 
+    private void OnSceneGUI()
+    {
+        // Draw a grid handle at the position of the grid point
+        Vector3 gridPoint = new Vector3(1, 0, 1);
+        Handles.DrawWireDisc(gridPoint, Vector3.up, 0.1f);
+
+        // Check if the grid point has been clicked
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.point == gridPoint)
+                {
+                    // Code to execute when the grid point is clicked
+                    Debug.Log("Handles.DrawWireDisc");
+                }
+            }
+        }
+    }
+
     public override void OnInspectorGUI()
     {
         // Draw the default inspector
@@ -37,12 +59,19 @@ public class MMapEditor : Editor
         // Draw the button to generate the map
         if (GUILayout.Button("Generate Map"))
         {
+            if (mMap.mapObject == null)
+            {
+                EditorGUILayout.HelpBox("Map object is null!", MessageType.Error);
+                return;
+            }
             mMap.GenerateMap();
         }
         if (GUILayout.Button("Clear Map"))
         {
             mMap.ClearMap();
         }
+        // Draw the property to modify the selected map
+        EditorGUILayout.PropertyField(mMapObject.FindProperty("selectedMap"));
 
         // Apply any changes to the serializedObject
         mMapObject.ApplyModifiedProperties();
