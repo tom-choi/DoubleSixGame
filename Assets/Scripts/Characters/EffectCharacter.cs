492ac61fd11ab37f842229867102e2f92292bbaa
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-04-10 01:03:20 +0800

    Update EffectCharacter.cs

--- Assets/Scripts/Characters/EffectCharacter.cs
+++ Assets/Scripts/Characters/EffectCharacter.cs
@@ -3,11 +3,4 @@ public class EffectCharacter
     private int strength;
     private int defense;
     private int speed;
-
-    public EffectCharacter(string name, int health, int strength, int defense, int speed) : base(name, health)
-    {
-        this.strength = strength;
-        this.defense = defense;
-        this.speed = speed;
-    }
 } 
