68c4e949decece0e701c74e7a71ea8b015b91912
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-04-09 13:26:52 +0800

    camera control system
    The LateUpdate() function is called after all other updates have been made, ensuring that the camera's position is updated after the target object has moved. The function first checks if the player has pressed the "Q" key to toggle mouse control. If mouse control is enabled, the camera's rotation is updated based on the player's mouse input. If mouse control is disabled, the camera's position is updated based on the player's input.
    
    The player's input is first rotated by the camera's current rotation using Quaternion.Euler(). The rotated input is then used to calculate a new position for the camera. The Mathf.Clamp() function is used to ensure that the camera's position stays within a certain range.

--- Assets/Scripts/GameManeger/CameraController.cs
+++ Assets/Scripts/GameManeger/CameraController.cs
@@ -4,7 +4,7 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.0f;
     [SerializeField] private Transform _target;
-
+    [SerializeField] private bool _isMouseControlEnabled = false;
     private Vector3 _offset;
 
     private void Start()
@@ -14,16 +14,33 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _isMouseControlEnabled = !_isMouseControlEnabled;
+            Cursor.visible = !_isMouseControlEnabled;
+            Cursor.lockState = _isMouseControlEnabled ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+
+        if (_isMouseControlEnabled)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            Vector3 newRotation = transform.eulerAngles + new Vector3(-mouseY, mouseX, 0);
+            transform.eulerAngles = newRotation;
+        }
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 newPosition = transform.position + new Vector3(horizontalInput, 0, verticalInput) * _speed * Time.deltaTime;
+        // Rotate the input vector by the camera's eulerAngles
+        Vector3 rotatedInput = Quaternion.Euler(transform.eulerAngles) * new Vector3(horizontalInput, 0, verticalInput);
+
+        Vector3 newPosition = transform.position + rotatedInput * _speed * Time.deltaTime;
 
         newPosition.x = Mathf.Clamp(newPosition.x, -10f, 10f);
         newPosition.z = Mathf.Clamp(newPosition.z, -10f, 10f);
 
         transform.position = newPosition;
-        // transform.position = Vector3.Lerp(transform.position, newPosition + _offset, Time.deltaTime * 10f);
-        
     }
 }
