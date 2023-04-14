5ef17ffc9a261425e4389e4452b234a29089992b
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-04-14 13:00:41 +0800

    Xnode 1.8.0 install and a demo

---
+++ Assets/Scripts/Xnode/NewNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class NewNode : Node {
+
+	// Use this for initialization
+	[Input] public float value;
+    [Output] public float result;
+
+    public override object GetValue(NodePort port) {
+        // Check which output is being requested. 
+        // In this node, there aren't any other outputs than "result".
+        if (port.fieldName == "result") {
+            // Return input value + 1
+            return GetInputValue<float>("value", this.value) + 1;
+        }
+        // Hopefully this won't ever happen, but we need to return something
+        // in the odd case that the port isn't "result"
+        else return null;
+    }
+}
