da11ffaafb98974116a97de9cfe5ba39bc25642f
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-05-17 17:17:40 +0800

    點撃玩家出現菜單

--- Assets/Scripts/RayCasterCam/RayCasterCam.cs
+++ Assets/Scripts/RayCasterCam/RayCasterCam.cs
@@ -1,17 +1,71 @@
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class RayCasterCam : MonoBehaviour
 {
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+    public List<GameObject> targets; // 选中的游戏对象
+    public List<GameObject> loseFocus; // 失焦的游戏对象
+    private RaycastHit hit; // 碰撞信息
+    public Material OutlineEffect;
+ 
+    private void Start() {
+        targets = new List<GameObject>();
+        loseFocus = new List<GameObject>();
+        hit = new RaycastHit();
+    }
+ 
+    private void Update() {
+        if (Input.GetMouseButtonUp(0)) 
         {
-            Shoot();
+            GameObject hitObj = GetHitObj();
+            if (hitObj == null) 
+            { 
+                // 未选中任何物体, 已选中的全部取消选中
+                targets.ForEach(obj => loseFocus.Add(obj));
+                targets.Clear();
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) 
+            {
+                if (targets.Contains(hitObj)) 
+                { // Ctrl重复选中, 取消loseFocus
+                    loseFocus.Add(hitObj);
+                    targets.Remove(hitObj);
+                } 
+                else 
+                { // Ctrl追加
+                    targets.Add(hitObj);
+                }
+            } 
+            else 
+            { // 单选
+                targets.ForEach(obj => loseFocus.Add(obj));
+                targets.Clear();
+                targets.Add(hitObj);
+                loseFocus.Remove(hitObj);
+            }
+            // do sth
+            DoSth();
+            ClearMyLoseFocus();
         }
     }
-    public string Shoot()
+    private void DoSth()
     {
+        TabMenu tabMenu = GameObject.Find("TabMenu").GetComponent<TabMenu>();
+        if (targets.Count == 1 && targets[0].name == "Player")
+        {
+            tabMenu.TabFlag = true;
+        }
+    }
+ 
+    private void ClearMyLoseFocus() 
+    { 
+        // Clear
+        loseFocus.Clear();
+    }
+ 
+    private GameObject GetHitObj() 
+    { // 获取屏幕射线碰撞的物体
         // Get the camera component attached to this game object
         Camera cam = GetComponent<Camera>();
 
@@ -21,14 +75,9 @@ public class RayCasterCam : MonoBehaviour
 
         // Check for collisions with objects in the scene
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            // Return the name of the game object that was hit
-            Debug.Log(hit.collider.gameObject.name);
-            return hit.collider.gameObject.name;
+        if (Physics.Raycast(ray, out hit)) {
+            return hit.collider.gameObject;
         }
-
-        // If no collision was detected, return null
         return null;
     }
 }
